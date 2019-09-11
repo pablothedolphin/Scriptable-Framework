5086269b7909554568895adf2f4462bd0ecb1a3a
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2019-09-11 09:16:28 +0100

    1.2.9: Added missing xml comments for extensions classes. Updated event documentation to describe how to use the new raise from ui api. Regenerated docfx website.

--- Scriptable Framework/Assets/Scriptable Framework/API/Misc/Extensions.cs
+++ Scriptable Framework/Assets/Scriptable Framework/API/Misc/Extensions.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 namespace ScriptableFramework
 {
 	/// <summary>
-	/// 
+	/// A collection of useful extension methods that probably should've already existed in 
+	/// the base .NET/Unity APIs.
 	/// </summary>
 	public static class Extensions
 	{
 		/// <summary>
-		/// 
+		/// Call <c>DestroyImmediate ()</c> on each direct child of this Transform.
 		/// </summary>
-		/// <param name="parent"></param>
+		/// <param name="parent">The parent of the objects to destroy.</param>
 		public static void DestroyChildren (this Transform parent)
 		{
 			for (int i = parent.childCount - 1; i >= 0; i--)
@@ -23,11 +24,11 @@ namespace ScriptableFramework
 		}
 
 		/// <summary>
-		/// 
+		/// Converts the current enum value into a string.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="value"></param>
-		/// <returns></returns>
+		/// <typeparam name="T">The enum type.</typeparam>
+		/// <param name="value">The current instance of the enum value.</param>
+		/// <returns>The enum value converted into a string.</returns>
 		public static string GetName<T> (this T value) where T : struct, IConvertible => Enum.GetName (typeof (T), value);
 	}
 }
